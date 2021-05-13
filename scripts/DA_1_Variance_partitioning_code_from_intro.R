e8a3384d839b6d6b6a78f19dde08e0e160620954
srg101 <sonya.geange@anu.edu.au>
2021-05-13 15:55:54 +0200

    Initial commit of code from our introduction

---
+++ scripts/DA_1_Variance_partitioning_code_from_intro.R
@@ -0,0 +1,7 @@
+m.SLA<-lmer(log(SLA)~1+(1|Species/Individual/Leaves)+(1|Site/Plot),data=d,na.action=na.omit)
+variances.SLA<-c(unlist(lapply(VarCorr(m.SLA),diag)),attr(VarCorr(m.LMA),”sc”)^2)
+#raw variances
+Variances.SLA
+#%Variance 
+(var.comp.SLA<-variances.SLA/sum(variances.SLA))
+# this is the same as: print(VarCorr(m.LMA),comp=”Variance”)
