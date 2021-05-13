ec01e8d56e888e9f8e068ff8e3ceb1d1f75e2802
tjgauthi <tjgauthi@uwaterloo.ca>
2021-05-13 09:42:04 -0400

    Deleting Processed data file

--- scripts/PFTC5_VariancePartitioning.R
+++ scripts/PFTC5_VariancePartitioning.R
@@ -24,8 +24,6 @@ intraspecific_data$Leaf_Thickness_AVG <-rowMeans(cbind (x1 = intraspecific_data$
 
 
 
-
-
 #partitioning of variance
 #varcomp.Height <- varcomp(lme(log(LMA),random=1|Elevation/Species/intraspecific/intraindividual, data = data, na.action = na.omit))
 
