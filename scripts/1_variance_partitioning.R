56b88c1c9c562b66df3471f59d78eb0ceb9e9047
Jon <henn.jonathan@gmail.com>
2022-02-17 11:27:22 -0700

    initial varpart analyses and graph

--- scripts/1_variance_partitioning.R
+++ scripts/1_variance_partitioning.R
@@ -15,8 +15,8 @@ blank_theme <- theme(panel.grid.major = element_blank(), #removes major axis gri
                      panel.background = element_blank(), #removes the default grey background
                      legend.key = element_blank(), #removes background behind legend keys
                      axis.line = element_line(colour = "black"), #makes axis lines black
-                     text = element_text (size = 27), #sets all text size to 20 
-                     axis.text = element_text(size = 24)) #sets axis text to size 15  
+                     text = element_text (size = 15), #sets all text size to 20 
+                     axis.text = element_text(size = 12)) #sets axis text to size 15  
 
 
 #check out how many leaves each individual has
@@ -27,46 +27,69 @@ leaf_count <- traits_wide %>%
 table(leaf_count$n)
 
 #need to decide on how to deal with individuals with 1-2 leaves
+traits %>% 
+  ggplot(aes(x = value)) +
+  geom_histogram() +
+  facet_wrap(~trait, scales = "free")
 
-#This code all comes from Julie Messier's web site
-mod<-lmer(log(leaf_thickness_mm)~1+(1|taxon/individual_uid)+(1|site), 
-          data=traits_wide, 
-          na.action=na.omit)
-variances<-c(unlist(lapply(VarCorr(mod),diag)), 
-             attr(VarCorr(mod),"sc")^2) #get variances
+#in traits, we need to log transform the traits that should be transformed, and create individual uid column.
+traits_log <- traits %>% 
+  mutate(value = log(value))
 
-#this is the same as: print(VarCorr(mod),comp="Variance")
+output <- data.frame(NULL)
+for(i in unique(traits_log$trait)){
+  #This code all comes from Julie Messier's web site
+  mod<-lmer(value~1+(1|functional_group/taxon/individual_nr)+(1|site), 
+            data=traits %>% filter(trait == i), 
+            na.action=na.omit)
+  variances<-c(unlist(lapply(VarCorr(mod),diag)), 
+               attr(VarCorr(mod),"sc")^2) #get variances
+  
+  var.comp<-variances/sum(variances)
+  
+  var.comp<-as.data.frame(var.comp) #creates a dataframe from the values
+  var.comp<-cbind(rownames(var.comp),data.frame(var.comp,row.names=NULL)) #changes row names into a column
+  var.comp<-melt(var.comp,value.name="value") #makes var.comp into a variable
+  names(var.comp)[1]<-"Scale" #changes the first column name to "scale"
+  
+  var.comp$value<-var.comp$value *100 #changes values into % 
+  
+  var.comp<- var.comp %>%
+    mutate(Scale = plyr::mapvalues(Scale, from = c(""), to = c("Unexplained"))) %>% 
+    mutate(Scale = factor(Scale, levels = c("Unexplained", "site.(Intercept)","individual_nr:(taxon:functional_group).(Intercept)", "taxon:functional_group.(Intercept)","functional_group.(Intercept)"))) %>% 
+    # group_by(variable)%>%
+    arrange(variable, Scale)%>%
+    mutate(labypos=100-(cumsum(value)-0.5*value)) %>%
+    #subset(value>1) %>% #this line removes variance partitioning less than 1% so that there are no zero labels
+    mutate(trait = i)
+  
+  output <- bind_rows(output, var.comp)
+}
 
-# raw variance
-variances
+# 
+# #this is the same as: print(VarCorr(mod),comp="Variance")
+# 
+# # raw variance
+# variances
+# 
+# # % Variance
+# 
+# (var.comp<-variances/sum(variances))
 
-# % Variance
 
-(var.comp<-variances/sum(variances))
 
-var.comp<-as.data.frame(var.comp) #creates a dataframe from the values
-var.comp<-cbind(rownames(var.comp),data.frame(var.comp,row.names=NULL)) #changes row names into a column
-var.comp<-melt(var.comp,value.name="value") #makes var.comp into a variable
-names(var.comp)[1]<-"Scale" #changes the first column name to "scale"
-
-var.comp$value<-var.comp$value *100 #changes values into % 
-
-var.comp<- var.comp %>%
-  group_by(variable)%>%
-  arrange(variable,desc(Scale))%>%
-  mutate(labypos=cumsum(value)-0.5*value)%>%
-  subset(value>1) #this line removes variance partitioning less than 1% so that there are no zero labels
-
-VP_Plot<-ggplot(var.comp, aes(x=variable, y=value))+
+VP_Plot<-ggplot(output, aes(x=trait, y=value))+
   geom_col(aes(fill=Scale))+
-  geom_text(aes(y=labypos, label=round(value,digits = 0)),colour="white", size = 10)+
+  geom_text(aes(y=labypos, label=round(value,digits = 0)),colour="white", size = 5)+
   #scale_fill_manual (values = scales_colours)+
   ylab("Proportion of Variance (%)")+
   xlab("")+
   blank_theme+
   labs(fill = "Ecological Scale")+
-  scale_y_continuous(expand=c(0,0),limits=c(0,100))+#this forces the graph to actually start at 0% and end at 100%
+  scale_y_continuous(expand=c(0,0),limits=c(0,100.1))+#this forces the graph to actually start at 0% and end at 100%
   #theme(legend.position = "none")+
-  scale_x_discrete(labels=function(x){sub("\\s", "\n", x)})
+  scale_x_discrete(labels=function(x){sub("\\s", "\n", x)}) +
+  scale_fill_manual(values = c("#EAC435", "#345995", "#03CEA4", "#FB4D3D", "#CA1551")) +
+  theme(axis.text.x = element_text(angle = 330, hjust = 0))
 VP_Plot
 
