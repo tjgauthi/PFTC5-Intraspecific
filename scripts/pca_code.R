587dfa5a825cbf51faabd9ec66fa3d385e8dd22f
mariapie <maria.elisa.pierfederici@nmbu.no>
2022-04-29 20:19:52 +0200

    update

--- scripts/pca_code.R
+++ scripts/pca_code.R
@@ -1,3 +1,8 @@
+#### Data analysis - Ordination ####
+
+## Authors: 
+
+#### Load libraries ####
 library(dplyr)
 library(ggplot2)
 library(devtools)
@@ -14,13 +19,14 @@ library(factoextra)
 # upload data 
 data <- read_csv("data/raw/PFTC3-Puna-PFTC5_Peru_2018-2020_LeafTraits_clean.csv")
 
-# select 2020 and sites (WAY, ACJ and TRE) 
+# select 2020, sites (WAY, ACJ and TRE), treatment and triats we are interest in 
 data <- data %>%
   filter(site %in% c("WAY", "ACJ", "TRE") &
            year == 2020 & 
            treatment == "C" &
            trait %in% c("plant_height_cm", "leaf_area_cm2", "sla_cm2_g", "ldmc", "dry_mass_g", "leaf_thickness_mm"))
 
+# should we log transform some of the traits?
 
 data$site <- factor(data$site)
 # data$site <- as.numeric(data$site)
@@ -66,33 +72,33 @@ ggbiplot(
 ) + theme_classic()
 
 
-fviz_pca_ind(pca_out, repel = TRUE,
-             col.var = "#2A2A2A", # Variables color
-             label = "none",
-             labelsize = 5, 
-             habillage = df1$functional_group,
-             addEllipses = TRUE,
-             ellipse.level = 0.95) +
-  theme_minimal(base_size = 15)
-
-
-
-# PCA - plot 2
-df2 <- data %>% select(c(site, taxon, individual_nr, functional_group, value))
-df2.trait <- factor(data$trait)
-pca_out1 <- prcomp(df2, center = TRUE, scale. = TRUE)
-summary(pca_out1)
-
-str(pca_out1)
-
-ggbiplot(pca_out1,choices=c(1,2))+ theme_classic()
-
-
-ggbiplot(
-  pca_out1,
-  scale = 0,
-  choices = c(1, 2),
-  # ellipse = TRUE,
-  groups = df1.trait
-) + theme_classic()
+# fviz_pca_ind(pca_out, repel = TRUE,
+#              col.var = "#2A2A2A", # Variables color
+#              label = "none",
+#              labelsize = 5, 
+#              habillage = df1$functional_group,
+#              addEllipses = TRUE,
+#              ellipse.level = 0.95) +
+#   theme_minimal(base_size = 15)
+# 
+# 
+# 
+# # PCA - plot 2
+# df2 <- data %>% select(c(site, taxon, individual_nr, functional_group, value))
+# df2.trait <- factor(data$trait)
+# pca_out1 <- prcomp(df2, center = TRUE, scale. = TRUE)
+# summary(pca_out1)
+# 
+# str(pca_out1)
+# 
+# ggbiplot(pca_out1,choices=c(1,2))+ theme_classic()
+# 
+# 
+# ggbiplot(
+#   pca_out1,
+#   scale = 0,
+#   choices = c(1, 2),
+#   # ellipse = TRUE,
+#   groups = df1.trait
+# ) + theme_classic()
 
